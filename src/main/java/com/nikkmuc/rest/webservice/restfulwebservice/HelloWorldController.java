9197f5358d90faa5296c969416f7fbc936348099
nikkscode <dominik.kisiala@gmail.com>
2022-08-13 12:49:56 +0200

    Created basic documentation example for /hello endpoint

--- src/main/java/com/nikkmuc/rest/webservice/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/nikkmuc/rest/webservice/restfulwebservice/HelloWorldController.java
@@ -1,12 +1,26 @@
 package com.nikkmuc.rest.webservice.restfulwebservice;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@RequestMapping(value="/hello")
+@Tag(name = "Hello")
+@ApiResponse(content = @Content(mediaType = "text/plain",
+        schema = @Schema(example = "Hello World")),
+        responseCode = "200",
+        description = "Successful Operation" )
+
 public class HelloWorldController {
 
-    @GetMapping("/hello")
+    @GetMapping
+    @Operation(
+            summary = "get a Hello World",
+            description = "Returns a Hello World in text/plain.")
     public String helloWorld(){
         return "Hello World";
     }
--- src/main/java/com/nikkmuc/rest/webservice/restfulwebservice/RestfulWebserviceApplication.java
+++ src/main/java/com/nikkmuc/rest/webservice/restfulwebservice/RestfulWebserviceApplication.java
@@ -1,7 +1,12 @@
 package com.nikkmuc.rest.webservice.restfulwebservice;
 
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class RestfulWebserviceApplication {
@@ -10,4 +15,12 @@ public class RestfulWebserviceApplication {
 		SpringApplication.run(RestfulWebserviceApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenApi(@Value("${app.version}") String appVersion, @Value("${app.name}") String appName){
+		return new OpenAPI()
+				.info(new Info()
+				.title(appName)
+				.description("This is a simple Restful Webservice for demo purpose.")
+				.version(appVersion));
+	}
 }
