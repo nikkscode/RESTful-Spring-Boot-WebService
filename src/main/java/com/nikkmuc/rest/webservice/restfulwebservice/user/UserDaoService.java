f6e03a1d1d7e24109a23b55fe6d1b4d59dc192f1
nikkscode <dominik.kisiala@gmail.com>
2022-08-13 19:33:41 +0200

    User Bean created with RestController and ExceptionHandling

---
+++ src/main/java/com/nikkmuc/rest/webservice/restfulwebservice/user/UserDaoService.java
@@ -0,0 +1,60 @@
+package com.nikkmuc.rest.webservice.restfulwebservice.user;
+
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users  = new ArrayList<>();
+    private static int usersCount = 5;
+
+    static {
+        users.add(new User(100, "Dave", new Date()));
+        users.add(new User(321, "Brian", new Date()));
+        users.add(new User(32, "Sarah", new Date()));
+        users.add(new User(313, "Martha", new Date()));
+        users.add(new User(90, "John", new Date()));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+
+     public User save(User user) {
+        if (user.getId()==null){
+            user.setId(++usersCount);
+        }
+        users.add(user);
+        return user;
+     }
+    public User findOne(int id){
+        User userTmp = null;
+        for(User user: users) {
+            if(user.getId()==id){
+                userTmp = user;
+            }
+        }
+        return userTmp;
+    }
+
+    public User deleteById(int id){
+        Iterator<User> iterator = users.iterator();
+        while (iterator.hasNext()){
+            User userTmp = iterator.next();
+            if (userTmp.getId() == id){
+                iterator.remove();
+                return userTmp;
+            }
+        }
+        return null;
+    }
+
+
+
+}
---
+++ src/main/java/com/nikkmuc/rest/webservice/restfulwebservice/user/UserResource.java
@@ -0,0 +1,57 @@
+package com.nikkmuc.rest.webservice.restfulwebservice.user;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@Tag(name = "User")
+@RequestMapping(path = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserDaoService service;
+
+    @GetMapping
+    @Operation(summary = "Get all Users", description = "Returns a list of all Users.")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    @Operation(summary = "Get User by ID", description = "Returns a User for a given ID.")
+    public User retrieveUser(@PathVariable int id){
+        User user = service.findOne(id);
+        if(user==null)
+            throw new UserNotFoundException("id-" + id);
+        return user;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @Operation(summary = "Delete User by ID", description = "Deletes a User for a given ID.")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user==null)
+            throw new UserNotFoundException("id-" + id);
+    }
+
+    @PostMapping
+    @Operation(summary = "Create new User", description = "Creates a new User.")
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        User tmpUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(tmpUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
